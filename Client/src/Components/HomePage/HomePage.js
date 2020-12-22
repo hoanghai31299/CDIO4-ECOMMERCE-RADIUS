fe210fc91625e6e211c4cfb33b3296fa0e292510
hoanghai31299 <hoanghai31299@gmail.com>
2020-12-22 17:44:27 +0700

    home-page done

--- Client/src/Components/HomePage/HomePage.js
+++ Client/src/Components/HomePage/HomePage.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomePage.css";
+import { Link } from "react-router-dom";
 function HomePage() {
+  const [mute, setMute] = useState(true);
   const arrLinks = [
     "https://res.cloudinary.com/hoanghai/video/upload/v1608572979/Radius-E/Bg-video/video_bg1_txtqlw.mp4",
     "https://res.cloudinary.com/hoanghai/video/upload/v1608572983/Radius-E/Bg-video/video_bg2_loq2pm.mp4",
@@ -9,13 +11,24 @@ function HomePage() {
   ];
   return (
     <div className="home-page">
-      {arrLinks.map((link) => {
-        return (
-          <video width="100%" autoplay muted loop autoPlay id="myVideo">
-            <source src={link} type="video/mp4" />
-          </video>
-        );
-      })}
+      <div className="video">
+        {arrLinks.map((link) => {
+          return (
+            <video width="100%" autoplay muted loop autoPlay>
+              <source src={link} type="video/mp4" />
+            </video>
+          );
+        })}
+      </div>
+      <div className="text">
+        <div onClick={() => setMute(!mute)} className="sound">
+          <span>{mute ? "SOUND ON" : "SOUND OFF"}</span>
+        </div>
+        <div className="link">
+          <Link to="/products/arrive">Arrive</Link>
+          <Link to="/products/bestseller"> Best Seller</Link>
+        </div>
+      </div>
     </div>
   );
 }
