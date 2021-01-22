79a7f0f469d93fc8d6622b536565c671c84df192
LinhTuong <linh_tuong@helloleotech.com>
2021-01-22 22:47:50 +0700

    Fix something

--- Server/controllers/notification.controller.js
+++ Server/controllers/notification.controller.js
@@ -90,7 +90,7 @@ exports.getNotification = async (req, res, next) => {
     const user = req.user;
     const { _id } = user;
     const notification = await Notification.find({
-      $or: [{ users: _id }, { users: [] }],
+      $or: [{ users: _id }, { users: [] }],deleteAt: undefined
     });
     if (!notification) {
       return res.status(400).json({
